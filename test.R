703ebac6e6911191961542dd96f574791012823f
jonatsgonats <jonatsgonats@gmail.com>
2017-11-22 07:44:40 +0800

    done

--- test.R
+++ test.R
@@ -4,41 +4,31 @@ myValues <- c(1:26)
 dataTable = data.frame(myNames,myValues)
 momCalculate <- function(dataTable,lookBackPeriod,sourceCol,targetCol)
 {
-  #dataTable[1,targetCol] <- dataTable[1,sourceCol]
-  dataTable[1:nrow(dataTable),targetCol] <- dataTable[1:nrow(dataTable),sourceCol]+1
-  
   n <- nrow(dataTable)
   
   for(i in 1:n)
   {
     if (i < lookBackPeriod)
     {
-     # dataTable[i,targetCol] <- 0
+      dataTable[i,targetCol] <- 0
     }
     if (i >= lookBackPeriod)
     {
-      loop <- 0
+    loop <- 0
       while (loop < lookBackPeriod)
       {
         if (i == (i-loop))
         {
-          dataTable[i,targetCol] <- dataTable[i,targetCol]
+          dataTable[i,targetCol] <- dataTable[i,sourceCol]+1
         }
         else
         {
-          dataTable[i,targetCol] <- dataTable[i,targetCol] + dataTable[(i-loop),targetCol]
+          dataTable[i,targetCol] <- (dataTable[i,targetCol]) + (dataTable[(i-loop),sourceCol]+1)
         }
         loop <- loop + 1
       }
     }
-    #dataTable[i,targetCol] <- (dataTable[i-1,targetCol] * dataTable[i,sourceCol])
-    
-    #if (i > lookBackPeriod)
-    #{
-    #  dataTable[i,targetCol] <- (dataTable[i,targetCol] / dataTable[i-lookBackPeriod,sourceCol])
-    #}
   }
-  
   return(dataTable)
 }
 
